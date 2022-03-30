ff08295d3db8e0b1684f2a35b50a6716a26aee63
andreshugueth <andreshugueth@gmail.com>
2022-03-29 21:37:49 -0500

    57 - login endpoint and login dependency

--- app/api.py
+++ app/api.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from app.common.controllers import user
+from app.auth.controllers import auth
 
 app = FastAPI(title="CRM API", version="0.0.1", description="API to crm app")
 
 
 app.include_router(user.router)
+app.include_router(auth.router)
 
 
 @app.get("/")
