1b8b969b85f5300ff57dc22e4ebe37700d87bfae
Jose A Vallejo <jose.vallejo@globant.com>
2022-04-23 12:01:36 -0500

    create alias for taxe_dto

--- app/quotes/schemas/tax_dto.py
+++ app/quotes/schemas/tax_dto.py
@@ -1,16 +1,16 @@
 from uuid import UUID
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class TaxBase(BaseModel):
-    tax_name: str
-    tax_value: float
-
+    tax_name: str = Field(..., alias="taxName")
+    tax_value: float = Field(..., alias="taxValue")
 
 
 class TaxResponse(TaxBase):
-    id: UUID
-    quote_id: UUID
+    id: UUID = Field(..., alias="id")
+    quote_id: UUID = Field(..., alias="quoteId")
 
     class Config:
         orm_mode = True
+        allow_population_by_field_name = True
