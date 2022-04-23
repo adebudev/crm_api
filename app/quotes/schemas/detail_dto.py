435a58812afd008ac544c08d7f7700fdbfbcdf27
Jose A Vallejo <jose.vallejo@globant.com>
2022-04-23 11:53:41 -0500

    create field alias for detail_dto

--- app/quotes/schemas/detail_dto.py
+++ app/quotes/schemas/detail_dto.py
@@ -1,22 +1,22 @@
 from uuid import UUID
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 
 class DetailBase(BaseModel):
-    valid_time: int
-    deliver_time: datetime
-    currency_type: Optional[str]
-    payment_terms: str
-    sub_total: float
-    total: float
-
+    valid_time: int = Field(..., alias="validTime")
+    deliver_time: datetime = Field(..., alias="deliverTime")
+    currency_type: Optional[str] = Field(None, alias="currencyType")
+    payment_terms: str = Field(..., alias="paymentTerms")
+    sub_total: float = Field(..., alias="subTotal")
+    total: float = Field(..., alias="total")
 
 
 class DetailResponse(DetailBase):
     id: UUID
-    created_at: datetime
+    created_at: datetime = Field(..., alias="createdAt")
 
     class Config:
         orm_mode = True
+        allow_population_by_field_name = True
