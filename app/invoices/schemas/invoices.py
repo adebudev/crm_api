e385d43c4aea01f018b0c2c02dbade6cc7a926d8
andreshugueth <andreshugueth@gmail.com>
2022-04-23 14:08:31 -0500

    invoices get and post methods

--- app/invoices/schemas/invoices.py
+++ app/invoices/schemas/invoices.py
@@ -1,22 +1,23 @@
+from datetime import datetime
+from typing import List, Optional
 from uuid import UUID
 
-from typing import List, Optional
-from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class InvoiceBase(BaseModel):
-    invoice_num: int
-    user_id: UUID
-    client_id: UUID
+    invoice_num: int = Field(..., alias="invoiceNumber")
+    user_id: UUID = Field(..., alias="userId")
+    client_id: UUID = Field(..., alias="clientId")
     detail: str
     total: float
 
 
 class InvoiceResponse(InvoiceBase):
     id: UUID
-    created_at: datetime
-    modified_on: datetime
+    created_at: datetime = Field(..., alias="createdAt")
+    modified_on: datetime = Field(..., alias="modifiedOn")
 
     class Config:
         orm_mode = True
+        allow_population_by_field_name = True
