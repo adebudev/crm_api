7cd5baf09f8ee2015ceda155cbd68bb69b1e906a
adasunci <ext_adasunci@mercadolibre.com.co>
2022-04-25 18:25:29 -0500

    58 - send email, reset password, more user fields

--- app/common/schemas/user.py
+++ app/common/schemas/user.py
@@ -2,16 +2,30 @@ from uuid import UUID
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
-
-class UserCreate(BaseModel):
+class UserBase(BaseModel):
+    first_name: str
+    second_name: str
+    last_name: str
     email: EmailStr
+    phone: str
+    address: str
+    country: str
+    city: str
+
+class UserCreate(UserBase):
     password: str
 
+class UserUpdatePassword(BaseModel):
+    password: str
 
-class UserResponse(BaseModel):
+class UserResponse(UserBase):
     id: UUID
-    email: EmailStr
     created_at: datetime
 
     class Config:
         orm_mode = True
+
+class UserResponseEmail(BaseModel):
+    status_code: int
+    message: str
+    email: EmailStr
