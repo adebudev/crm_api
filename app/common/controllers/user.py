4ad1b6b05dc333e62534357087bbaed10e1afd54
adasunci <ext_adasunci@mercadolibre.com.co>
2022-04-26 07:33:47 -0500

    58 - send email, reset password

--- app/common/controllers/user.py
+++ app/common/controllers/user.py
@@ -1,29 +1,25 @@
-from uuid import UUID
-from fastapi import status, HTTPException, Depends, APIRouter
-from sqlalchemy.orm import Session
-from app.common.database import get_db
-from app.common.schemas.user import UserCreate, UserResponse
-from app.common.models.user import User
+from fastapi import status, Depends, APIRouter
+from app.common.schemas.user import UserResponse, UserResponseEmail
+from app.common.repository.user import create, getById, send_email, update_password
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=UserResponse)
-def create_user(user: UserCreate, db: Session = Depends(get_db)):
-    new_user = User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+def create_user(user: UserResponse = Depends(create)):
+    return user
 
 
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=UserResponse)
-def get_user(id: UUID, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.id == id).first()
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="User with id: {} does not exist".format(id),
-        )
+def get_user(user: UserResponse = Depends(getById)):
+    return user
 
+
+@router.post('/send-email', status_code=status.HTTP_200_OK, response_model=UserResponseEmail)
+async def send_email_asynchronous(user_email: UserResponseEmail = Depends(send_email)):
+    return user_email
+
+
+@router.put('/reset-password/{id}', status_code=status.HTTP_200_OK, response_model=UserResponse)
+def new_password(user: UserResponse = Depends(update_password)):
     return user
